6d408ab40e9652a14cc6cbb72ba7ab9997874e0e
minjunp <mp44@rice.edu>
2020-10-27 19:59:22 -0500

    updated Oct27

--- code/main_entry.py
+++ code/main_entry.py
@@ -1,10 +1,8 @@
-
 import pandas as pd
 import matplotlib.pyplot as plt
 from preprocess import process_main, unscale_minMax, unscale_log
 from training import training_main
 import numpy as np
-
 
 startDate = '2015-10-10'
 endDate = '2019-11-19'
@@ -12,11 +10,10 @@ feature_list = ['High', 'Low']
 window_len = 7
 if_stock = False
 
-small_num = 10**-10 # really small number to handle the 0's before log-scaling 
+small_num = 10**-10 # really small number to handle the 0's before log-scaling
 if_minmax_scale = False		# option for using min max scaling
 if_log_scale = True    # option for using logarithm scale
 if_percent_change = True     # option for percentage return
-
 
 # Read & Transform csv files into dataframes for preprocessing
 bitcoin_daily_path = "./data/cryptocurrency/daily(2015-2019)/bitcoin_daily_10102015-11232019.csv"
@@ -26,7 +23,6 @@ ether_hourly_path = "./data/cryptocurrency/hourly(2019)/Etherum_hourly_2019.csv"
 
 # stock
 stock_daily_path = "./data/stock/SPDaily101015.csv"
-
 
 filePath = [bitcoin_daily_path, bitcoin_hourly_path, ether_daily_path, ether_hourly_path, stock_daily_path] # change this file name to other files
 plotNames = ['bitcoin_daily', 'bitcoin_hourly', 'etherum_daily', 'etherum_hourly', 'stock_daily']
@@ -40,7 +36,7 @@ for i in range(len(filePath)):
 
 	scaled_df, scaler, original_df = process_main(startDate, endDate, filePath[i], if_minmax_scale, if_log_scale, small_num)
 
-	# begin training 
+	# begin training
 	prediction, history, test_indx, model = training_main(scaled_df, if_stock)
 	test_dateTime_indx = test_indx[window_len:] # 7 is the window size
 
@@ -49,7 +45,7 @@ for i in range(len(filePath)):
 	if if_minmax_scale:
 		if if_stock:
 			unscaled_pred_df = unscale_minMax(prediction, original_df, ['Low', 'High'])
-		
+
 		else:
 			unscaled_pred_df = unscale_minMax(prediction, original_df, feature_list)
 
@@ -68,7 +64,7 @@ for i in range(len(filePath)):
 	# draw a line plot of pred vs. actual
 	test_old_df = original_df.loc[test_dateTime_indx[0]: test_dateTime_indx[-1]]
 
-	if if_log_scale and if_percent_change: 
+	if if_log_scale and if_percent_change:
 		test_old_df = test_old_df.apply(lambda x: np.log(x)-np.log(x.shift(1)))
 
 	plot_df = test_old_df[['High', 'Low']].copy()
@@ -83,18 +79,14 @@ for i in range(len(filePath)):
 	# plot and save the line plot
 	plt.tight_layout(pad=0.4, w_pad=0.5)
 	plot1 = plot_df[['High', 'High_truth']].plot.line(figsize=(8,5))
-	plot1.set_title(plotNames[i]+'High', fontsize=15) 
+	plot1.set_title(plotNames[i]+'High', fontsize=15)
 	plot1.set_ylabel("Percentage change")
 	fig1 = plot1.get_figure()
 	fig1.savefig(plotNames[i] + '_high_' + '.png')
 
 	plt.tight_layout(pad=0.4, w_pad=0.5)
 	plot2 = plot_df[['Low', 'Low_truth']].plot.line(figsize=(7,5))
-	plot2.set_title(plotNames[i]+'Low', fontsize=15) 
+	plot2.set_title(plotNames[i]+'Low', fontsize=15)
 	plot2.set_ylabel("Percentage change")
 	fig2 = plot2.get_figure()
 	fig2.savefig(plotNames[i] + '_low_' +'.png')
-
-
-
-
--- code/training.py
+++ code/training.py
@@ -9,15 +9,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, Flatten, Dense, AveragePooling1D, BatchNormalization, Activation, LSTM, Dropout, concatenate
 from tensorflow import keras
 
-register_matplotlib_converters()
-
-endpoint = 'https://min-api.cryptocompare.com/data/histoday'
-# n days interval
-res = requests.get(endpoint + '?fsym=BTC&tsym=USD&limit=200')
-hist = pd.DataFrame(json.loads(res.content)['Data'])
-hist = hist.set_index('time')
-hist.index = pd.to_datetime(hist.index, unit='s')
-
 """get 200 days from 2012–10–10 until today
 we can print each column by using . """
 
@@ -37,7 +28,6 @@ def line_plot(line1, line2, label1=None, label2=None, title=''):
     ax.legend(loc='best', fontsize=18)
     plt.show()
 
-train, test = train_test_split(hist, test_size=0.1)
 """visualize how dataset is split"""
 #line_plot(train.close, test.close, 'training', 'test', 'BTC')
 
@@ -55,7 +45,7 @@ def normalise_zero_base(df):
     """
     return df / df.iloc[0] - 1
 
-def extract_window_data(df, window=7, zero_base=True):
+def extract_window_data(df, window=7, zero_base=False):
     """ Convert dataframe to overlapping sequences/windows of
         length `window`.
     """
@@ -67,23 +57,24 @@ def extract_window_data(df, window=7, zero_base=True):
         window_data.append(tmp.values)
     return np.array(window_data)
 
-def prepare_data(df, window=7, zero_base=True, test_size=0.1):
+def prepare_data(df, window=7, zero_base=False, test_size=0.1):
     """ Prepare data for LSTM. """
     # train test split
+    # window size = len(test_size)
     train_data, test_data = train_test_split(df, test_size)
 
     # extract window data
     X_train = extract_window_data(train_data, window, zero_base)
     X_test = extract_window_data(test_data, window, zero_base)
 
-    y_train_close = train_data.close[window:].values
-    y_test_close = test_data.close[window:].values
-    y_train_high = train_data.high[window:].values
-    y_test_high = test_data.high[window:].values
-    y_train_low = train_data.low[window:].values
-    y_test_low = test_data.low[window:].values
-    y_train_open = train_data.open[window:].values
-    y_test_open = test_data.open[window:].values
+    y_train_close = train_data.Close[window:].values
+    y_test_close = test_data.Close[window:].values
+    y_train_high = train_data.High[window:].values
+    y_test_high = test_data.High[window:].values
+    y_train_low = train_data.Low[window:].values
+    y_test_low = test_data.Low[window:].values
+    y_train_open = train_data.Open[window:].values
+    y_test_open = test_data.Open[window:].values
 
 
     if zero_base:
@@ -98,32 +89,21 @@ def prepare_data(df, window=7, zero_base=True, test_size=0.1):
 
     return train_data, test_data, X_train, X_test, y_train_close, y_test_close, y_train_high, y_test_high, y_train_low, y_test_low, y_train_open, y_test_open
 
-train, test, X_train, X_test, y_train_close, y_test_close, y_train_high, y_test_high, y_train_low, y_test_low, y_train_open, y_test_open = prepare_data(hist)
-
-def build_lstm_model_ver1(input_data, output_size, neurons=20,
-                     activ_func='linear', dropout=0.25,
-                     loss='mae', optimizer='adam'):
-    model = Sequential()
-    model.add(LSTM(neurons, input_shape=(input_data.shape[1], input_data.shape[2])))
-    model.add(Dropout(dropout))
-    model.add(Dense(units=output_size))
-    model.add(Activation(activ_func))
-    model.compile(loss=loss, optimizer=optimizer)
-    return model
-
-input_data = hist
-
 def create_model():
     first_input = keras.Input(shape=(7,2), name = 'first_input')
     second_input = keras.Input(shape=(7,2), name = 'second_input')
 
-    first_layer = LSTM(20)
+    first_layer_1 = LSTM(256)
+    first_layer_2 = LSTM(256)
     #first_layer = Conv1D(filters=30, kernel_size=2, data_format='channels_last', input_shape=(7,2))
 
-    first = first_layer(first_input)
-    second = first_layer(second_input)
+    first = first_layer_1(first_input)
+    second = first_layer_2(second_input)
+
     x = concatenate([first, second])
-    #x = Dropout(0.25)(x)
+
+    x = Dropout(0.21)(x)
+
     output1 = Dense(1, activation='linear', name = 'output1')(x)
     output2 = Dense(1, activation='linear', name = 'output2')(x)
 
@@ -132,45 +112,42 @@ def create_model():
     model.summary()
 
     #plot the models
-    #keras.utils.plot_model(model, 'multi_input_and_output_model.png', show_shapes=True)
+    #keras.utils.plot_model(model, 'nn_model.png', show_shapes=True)
 
     # for single outputs
     #model.compile(loss='mae', optimizer='adam')
     # for two outputs
-    model.compile(optimizer='adam', loss={'output1': 'mae', 'output2': 'mae'}, loss_weight=[1., 0.2])
+    model.compile(optimizer='adam', loss={'output1': 'mse', 'output2': 'mse'}, loss_weight=[1., 1.])
 
     return model
 
 
-model = create_model()
+def training_main(passed_df, if_stock):
+    """
+    Entry point to all the functions in this file. Used for training.
+    """
+    print("Training on the passed in dataset...")
 
-x1_train = X_train[:,:,[0,3]]
-x2_train = X_train[:,:,[1,2]]
-x1_test = X_test[:,:,[0,3]]
-x2_test = X_test[:,:,[1,2]]
+    register_matplotlib_converters()
 
-#history = model.fit({'first_input': x1_train, 'second_input': x2_train}, y_train_low, epochs=10, batch_size=4, validation_split=0.1)
-model.fit({'first_input': x1_train, 'second_input': x2_train}, {'output1': y_train_low,'output2': y_train_high}, epochs=10, batch_size=4, validation_split=0.1)
-sys.exit()
-history2 = model.evaluate({'first': x1_test, 'second': x2_test}, {'output1': y_test_low,'output2': y_test_high})
-pred = model.predict({'forward': x1_test, 'backward': x2_test})
+    hist = passed_df
+    model = create_model()
+    train, test = train_test_split(hist, test_size=0.1)
 
-sys.exit()
-#targets = test[target_col][window:]
-preds = model.predict(X_test).squeeze()
-history2 = model.evaluate(X_test, y_test)
+    train, test, X_train, X_test, y_train_close, y_test_close, y_train_high, y_test_high, y_train_low, y_test_low, y_train_open, y_test_open = prepare_data(hist, zero_base=False)
 
-print(preds)
-print(history2)
-"""
-we are reporting the loss - mean squared error - and this is very low, meaning
-predicting very well..
-"""
-sys.exit()
-# convert change predictions back to actual price
-"""
-preds = test.close.values[:-window] * (preds + 1)
-preds = pd.Series(index=targets.index, data=preds)
-n = 30
-line_plot(targets[-n:], preds[-n:], 'actual', 'prediction')
-"""
+    x1_train = X_train[:,:,[0,3]]
+    x2_train = X_train[:,:,[1,2]]
+    x1_test = X_test[:,:,[0,3]]
+    x2_test = X_test[:,:,[1,2]]
+
+    #history = model.fit({'first_input': x1_train, 'second_input': x2_train}, y_train_low, epochs=10, batch_size=4, validation_split=0.1)
+    if not if_stock:
+        model.fit({'first_input': x1_train, 'second_input': x2_train}, {'output1': y_train_low,'output2': y_train_high}, epochs=30, batch_size=200, validation_split=0.1)
+        print('********************')
+    # sys.exit()
+    history2 = model.evaluate({'first_input': x1_test, 'second_input': x2_test}, {'output1': y_test_low,'output2': y_test_high})
+    pred = model.predict({'first_input': x1_test, 'second_input': x2_test})
+
+    print("...and done!")
+    return pred, history2, test.index, model
